151cb1acff5b46222bc20a537892ef47ef96d084
algourdinos <nalgourdin@ensegid.fr>
2025-11-12 15:24:09 +0100

    import biblio

--- etape1/etape1.py
+++ etape1/etape1.py
@@ -1,7 +1,24 @@
+from laspy import *
+import numpy as np
+import matplotlib.pyplot as plt
+
 def etape1_main():
     print("Exécution de l'étape 1...")
     # Exemple : lecture d’un fichier ou génération de données
     data = [1, 2, 3]
     print("Input data is " + str(data) + ".")
     return data
+
+
+
+
+
+
+
+
+
+
+
+
+
 
