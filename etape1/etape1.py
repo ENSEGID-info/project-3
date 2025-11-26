7a45ccc7afa3f3596ee2db25f50567fabc9ca100
algourdinos <nalgourdin@ensegid.fr>
2025-11-26 17:04:54 +0100

    import de donnée no7

--- etape1/etape1.py
+++ etape1/etape1.py
@@ -14,17 +14,17 @@ def etape1_main():
 
 
 def ouverture_laspy():
-    with laspy.open('H:/github/project-3/project-3/DOM Sego Sandstone/H:/github/project-3/project-3/DOM Sego Sandstone/2504_SEGO_NewMorvan_subset_filtered_03_percent.las') as fh:
+    with laspy.open('H:/Données de la prof/2504_SEGO_NewMorvan_subset_filtered_03_percent.las') as fh:
         
         print('Points from Header:', fh.header.point_count)
-    las = fh.read()
-    print(las)
-    print('Points from data:', len(las.points))
-    ground_pts = las.classification == 2
-    bins, counts = np.unique(las.return_number[ground_pts], return_counts=True)
-    print('Ground Point Return Number distribution:')
-    for r,c in zip(bins,counts):
-        print('    {}:{}'.format(r,c))
+        las = fh.read()
+        print(las)
+        print('Points from data:', len(las.points))
+        ground_pts = las.classification == 2
+        bins, counts = np.unique(las.return_number[ground_pts], return_counts=True)
+        print('Ground Point Return Number distribution:')
+        for r,c in zip(bins,counts):
+            print('    {}:{}'.format(r,c))
 
 
 
