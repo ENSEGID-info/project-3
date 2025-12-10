f59e8b4b2b71f20ce7b82306e1eff96229038587
tinou3324 <antoine.bouiges@bordeaux-inp.fr>
2025-12-10 11:28:56 +0100

    docstring 2

--- etape1/etape1.py
+++ etape1/etape1.py
@@ -5,26 +5,58 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def etape1_main():
-    print("Exécution de l'étape 1...")            # Exemple : lecture d’un fichier ou génération de données
+    """
+    Exécute l'étape 1 du programme.
+
+    Cette fonction affiche un message indiquant l'exécution de l'étape,
+    génère une liste de données simples et la retourne.
+
+    :return: Une liste de données d'exemple [1, 2, 3].
+    :rtype: list
+    """
+    print("Exécution de l'étape 1...")            
+ # Exemple : lecture d’un fichier ou génération de données
     data = [1, 2, 3]
     print("Input data is " + str(data) + ".")
     return data
 
+# Fonction d'ouverture d'un fichier LAS via LasPy
+# Elle lit les points, affiche des informations du header
+# et retourne l'objet LAS pour une utilisation ultérieure
 
 
 def ouverture_laspy():
+    """
+   Ouvre un fichier LAS et affiche plusieurs informations utiles.
+
+   La fonction utilise la bibliothèque LasPy pour lire un fichier LAS.
+   Elle affiche :
+     - Le nombre total de points selon l'en-tête,
+     - Les données générales du fichier,
+     - Le nombre réel de points lus,
+     - La distribution des retours (return number) des points classés comme sol.
+
+   :return: L'objet LAS contenant toutes les données du fichier.
+   :rtype: laspy.LasData
+   """
     with laspy.open('H:\Données de la prof/2504_SEGO_NewMorvan_subset_filtered_03_percent.las') as fh:    
         las = fh.read()
+# Affichage du nombre total de points dans le fichier
         print('Points from Header:', fh.header.point_count)
-       
+# Affichage du nombre de points réellement lus       
         print(las)
         print('Points from data:', len(las.points))
+# Extraction des points classés comme "sol" (classification = 2)
         ground_pts = las.classification == 2
         bins, counts = np.unique(las.return_number[ground_pts], return_counts=True)
         print('Ground Point Return Number distribution:')
         for r,c in zip(bins,counts):
             print('    {}:{}'.format(r,c))
     return las
+
+# Lecture du fichier LAS pour extraire ses données
+
+
 with laspy.open('H:\Données de la prof/2504_SEGO_NewMorvan_subset_filtered_03_percent.las') as fh:
       las = fh.read()          
 #liste des cordonnée
@@ -39,7 +71,7 @@ G=las.green/256
 #plt.show()
 
 
-    
+# Affichage d’un exemple de point (coordonnées + couleurs)
     
         
 
@@ -62,7 +94,12 @@ with laspy.open('H:\Données de la prof/2504_SEGO_NewMorvan_subset_filtered_03_p
 point_format = las.point_format
 print(list(point_format.dimension_names))
 
+
+# Calcul du NDVI sur chaque point :
+# NDVI = (Green - Red) / (Green + Red - Blue)
+# Puis recoloration : si NDVI > 0.1, on colore le point en rouge
 #NDVI==(Green - Red)/(Green + Red - Blue)
+
 
 for k in range (len(X)):
     NDVI=(G[k]-R[k])/(G[k]+R[k]-B[k])
@@ -75,7 +112,9 @@ for k in range (len(X)):
             None
     else:
         None 
-
+        
+# Construction de la liste des couleurs RGB normalisées (0-1)
+# Utilisées par Matplotlib pour l'affichage
          
 C=[]
 for i in range(len(X)):
@@ -84,7 +123,9 @@ for i in range(len(X)):
     d.append(G[i]/255)
     d.append(B[i]/255)
     C.append(d)
-         
+ 
+# Affichage 3D du nuage de points avec coloration NDVI
+            
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
