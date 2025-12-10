4c20f60d0fc616d2e2f09c78af995fbe620374ca
algourdinos <nalgourdin@ensegid.fr>
2025-12-10 11:48:02 +0100

    fonction

--- etape1/etape1.py
+++ etape1/etape1.py
@@ -13,10 +13,20 @@ def etape1_main():
 
 
 def ouverture_laspy():
+    """
+    
+
+    Returns
+    -------
+    las : TYPE
+        DESCRIPTION.
+
+    """
     with laspy.open('H:\Données de la prof/2504_SEGO_NewMorvan_subset_filtered_03_percent.las') as fh:    
         las = fh.read()
         print('Points from Header:', fh.header.point_count)
-       
+        point_format = las.point_format
+        print(list(point_format.dimension_names))
         print(las)
         print('Points from data:', len(las.points))
         ground_pts = las.classification == 2
@@ -24,105 +34,74 @@ def ouverture_laspy():
         print('Ground Point Return Number distribution:')
         for r,c in zip(bins,counts):
             print('    {}:{}'.format(r,c))
-    return las
-with laspy.open('H:\Données de la prof/2504_SEGO_NewMorvan_subset_filtered_03_percent.las') as fh:
-      las = fh.read()          
-#liste des cordonnée
-X=las.x
-Y=las.y
-Z=las.z
-#liste couleur RGB #couleur via lidar  /256
-R=las.red/256
-B=las.blue/256
-G=las.green/256
-#plt.plot(X,Y,Z)
-#plt.show()
+    return las 
 
-
+def co():
+    las =ouverture_laspy()
+    #liste des cordonnée
+    X=las.x
+    Y=las.y
+    Z=las.z
+    #liste couleur RGB #couleur via lidar  /256
+    R=las.red/256
+    B=las.blue/256
+    G=las.green/256
     
-    
-        
-
-print("Exemple de coordonnées :", list(zip(X, Y, Z,R,B,G))[:1])
+    return X,Y,Z,R,G,B
+def exemple():
+    return("Exemple de coordonnées :", list(zip(co()))[:1])
    
-#couleur disponible
-
-if hasattr(las, "red"):
-    r = las.red
-    g = las.green
-    b = las.blue
-    print("Couleurs disponibles")
-else:
-    print("Pas de couleur dans ce fichier")
-
-#formt d'un point
-with laspy.open('H:\Données de la prof/2504_SEGO_NewMorvan_subset_filtered_03_percent.las') as fh:
-     las = fh.read()
-        
-point_format = las.point_format
-print(list(point_format.dimension_names))
-
 #NDVI==(Green - Red)/(Green + Red - Blue)
+def couleur_arbre():
+    X,Y,Z,R,G,B=co()
+    for k in range (len(X)):
+        NDVI=(G[k]-R[k])/(G[k]+R[k]-B[k])
+        if Z[k]>1792:
+            if NDVI>0.1:#valeur de référence pour une végétation moderer 
+                G[k]=255
+                R[k]=0
+                B[k]=0
+            else :
+                None
+        else:
+            None 
+    return G,R,B
+            
+def liste_couleur():
 
-for k in range (len(X)):
-    NDVI=(G[k]-R[k])/(G[k]+R[k]-B[k])
-    if Z[k]>1792:
-        if NDVI>0.1:#valeur de référence pour une végétation moderer 
-            G[k]=255
-            R[k]=0
-            B[k]=0
-        else :
-            None
-    else:
-        None 
+    G,R,B= couleur_arbre()
+    C=[]
+    for i in range(len(G)):
+        d=[]
+        d.append(R[i]/255)
+        d.append(G[i]/255)
+        d.append(B[i]/255)
+        C.append(d)
+    return C
 
-         
-C=[]
-for i in range(len(X)):
-    d=[]
-    d.append(R[i]/255)
-    d.append(G[i]/255)
-    d.append(B[i]/255)
-    C.append(d)
-         
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+def sup_points():
+    return 
+        
+def affichage(): 
+    X,Y,Z,R,G,B=co()       
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    C=liste_couleur()
 
-ax.scatter(X, Y, Z,c=C, cmap='terrain', s=1)    # scatter 3D
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-elevation_angle = 25            # modifie angle de vue verticalement
-azimuthal_angle = -130            # modifie angle de vue horizontalement
-ax.view_init(elevation_angle, azimuthal_angle)
-plt.title("Nuage de points 3D")
-plt.show()
+    ax.scatter(X, Y, Z,c=C, cmap='terrain', s=1)     # scatter 3D
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    elevation_angle = 25            # modifie angle de vue verticalement
+    azimuthal_angle = -130            # modifie angle de vue horizontalement
+    ax.view_init(elevation_angle, azimuthal_angle)
+    plt.title("Nuage de points 3D")
+    plt.show()
+    return None
 
          
             
-# import numpy as np
-# import matplotlib.pyplot as plt
 
-# # Créer des données pour les axes x, y et z
-# z = Z
-# x = X
-# y = Y
-
-# # Créer un objet Axes3D pour le graphique 3D
-# plt.figure("Exemple de courbe en 3D")
-# axes = plt.axes(projection="3d")
-# print(axes, type(axes))
-
-# # Tracer les lignes en 3D
-# axes.plot(x, y, z)
-
-# # Ajouter des étiquettes pour les axes
-# axes.set_xlabel("X")
-# axes.set_ylabel("Y")
-# axes.set_zlabel("Z")
-
-# # Afficher le graphique en 3D
-# plt.show()
 
 
 
